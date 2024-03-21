a1bdce44b4541166aeaaebea610f10c2146edf54
DMR-max <60442394+DMR-max@users.noreply.github.com>
2024-03-21 02:30:26 +0100

    experiment mis werkend?

--- ShortCutAgents.py
+++ ShortCutAgents.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+from ShortCutEnvironment import ShortcutEnvironment as env
 
 class QLearningAgent(object):
 
@@ -8,17 +9,16 @@ class QLearningAgent(object):
         self.n_states = n_states
         self.epsilon = epsilon
         self.Q_value = np.zeros((n_states, n_actions))
-        print(self.Q_value)
         
     def select_action(self, state):
         random_float = np.random.random()
         if random_float < (1 - self.epsilon):
             
-            a = np.argmax(self.action_val)
+            a = np.argmax(self.Q_value[state])
 
         else:
             # choose argmax from list of action values
-            b = np.argmax(self.action_val)
+            b = np.argmax(self.Q_value[state])
             # take out the best action from list of actions
             probability = []
             for i in range(self.n_actions):
@@ -29,9 +29,10 @@ class QLearningAgent(object):
             a = int(np.random.choice(probability,size = 1))
         return a
         
-    def update(self, state, action, reward):
-        # TO DO: Add own code
-        pass
+    def update(self, env, new_state, state, action, reward):
+        gamma = 1.0
+        alpha = 0.1
+        self.Q_value[state, action] += alpha * (reward + gamma * np.max(self.Q_value[new_state])-self.Q_value[state, action])
 
 class SARSAAgent(object):
 
--- ShortCutExperiment.py
+++ ShortCutExperiment.py
@@ -1,5 +1,30 @@
 # Write your experiments in here! You can use the plotting helper functions from the previous assignment if you want.
 import numpy as np
+from ShortCutAgents import QLearningAgent, SARSAAgent, ExpectedSARSAAgent
+from ShortCutEnvironment import ShortcutEnvironment
+
+def run_repetitions(n_reps, n_steps):
+    rewards = np.zeros((n_reps, n_steps))
+    for rep in range(n_reps):
+        env = ShortcutEnvironment()
+        n_actions = env.action_size()
+        n_states = env.state_size()
+        epsilon = 0.1
+        agent = QLearningAgent(n_actions, n_states, epsilon)
+        env.reset()
+        state = env.state()
+        for step in range(n_steps):
+            if env.isdone:
+                env.reset()
+                state = env.state()
+            action = agent.select_action(state)
+            reward = env.step(action)
+            new_state = env.state()
+            agent.update(env, new_state, state, action, reward)
+            state = new_state
+            rewards[rep, step] = reward
+    print_greedy_actions(agent.Q_value)
+    return rewards
 
 def print_greedy_actions(Q):
     greedy_actions = np.argmax(Q, 1).reshape((12,12))
@@ -13,3 +38,22 @@ def print_greedy_actions(Q):
     line_breaks[:] = '\n'
     print_string = np.hstack((print_string, line_breaks))
     print(print_string.tobytes().decode('utf-8'))
+
+def main():
+    n_reps = 1
+    n_steps = 10000
+    rewards = run_repetitions(n_reps, n_steps)
+    print(rewards.mean(0))
+    
+    # agent = SARSAAgent(n_actions, n_states, epsilon)
+    # rewards = run_repetitions(agent, env, n_reps, n_steps)
+    # print_greedy_actions(agent.Q_value)
+    # print(rewards.mean(0))
+    
+    # agent = ExpectedSARSAAgent(n_actions, n_states, epsilon)
+    # rewards = run_repetitions(agent, env, n_reps, n_steps)
+    # print_greedy_actions(agent.Q_value)
+    # print(rewards.mean(0))
+
+if __name__ == '__main__':
+    main()
